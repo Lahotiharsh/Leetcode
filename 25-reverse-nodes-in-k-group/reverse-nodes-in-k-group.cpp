4a400245dff69fbf4de21d0558b97b6b9b8e74ee
Lahotiharsh <149718078+Lahotiharsh@users.noreply.github.com>
2025-05-16 00:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (38.33%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using cd = complex<double>;
+using pii = pair<int, int>;
+using psi = pair<string, int>;
+using pll = pair<ll, ll>;
+using ppii = pair<int, pii>;
+using vi = vector<int>;
+using vd = vector<double>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vc = vector<char>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using pis = pair<int, string>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using umii = unordered_map<int, int>;
+using umci = unordered_map<char, int>;
+#define pb push_back
+#define MOD 1000000007
+#define LMOD 1000000000000000007LL
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *reverseKGroup(ListNode *head, int k) {
+    int count = 0;
+    ListNode *temp = head;
+    while (temp && count < k) {
+      temp = temp->next;
+      count++;
+    }
+    if (count == k) {
+      count = 0;
+      ListNode *prev = NULL;
+      ListNode *curr = head;
+      ListNode *next = NULL;
+      while (curr && count < k) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+      }
+      if (next) {
+        head->next = reverseKGroup(curr, k);
+      }
+      return prev;
+    } else {
+      return head;
+    }
+  }
+};
